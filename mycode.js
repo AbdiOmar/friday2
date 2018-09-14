0ef3d4517e56bb64338b553198adcf900cf38bfe
AbdiOmar <abdiwahab1783@gmail.com>
2018-09-14 17:01:59 +0300

    add  style in css create my mycode.js add js code script in index.html

--- mycode.js
+++ mycode.js
@@ -1,1 +1,9 @@
-              // The function returns the product of p1 and p2
+var side1 = parseInt(prompt("Enter First Side"));
+var side2 = parseInt(prompt("Enter Second Side"));
+var side3 = parseInt(prompt("Enter Third Side"));
+if (side1==side2 && side1==side3 && side2 ==side3){
+  alert("it's an Equilateral triangle")
+}
+else if(side1==side2 || side2 == side3 || side3 == side1){
+  alert("it's an Isoceles Triangle")
+};
