6b5a513427e9e8015ba7e77015a3f70c59be6382
AbdiOmar <abdiwahab1783@gmail.com>
2018-09-20 15:53:33 +0300

    change style add ul list on html add new js code

--- mycode.js
+++ mycode.js
@@ -7,6 +7,9 @@ var side3 = parseInt(document.getElementById("Third_side").value);
 if (side1==side2 && side1==side3 && side2 ==side3){
   document.getElementById('message').innerHTML="it's an Equilateral Triangle";
 }
+else if(side1+side2<= side3 || side1+side3<= side2 || side2+side2<=side1){
+  document.getElementById('message').innerHTML="it's not a triangle"
+}
 else if(side1==side2 || side2 == side3 || side3 == side1){
   document.getElementById('message').innerHTML="it's an Isoceles Triangle";
 }
@@ -14,6 +17,6 @@ else if (side1!==side2!==side3){
   document.getElementById('message').innerHTML="It's a Scalene Triangle";
 }
   else{
-    document.getElementById('message').innerHTML="It's not Triangle";
+    document.getElementById('message').innerHTML="enter valid values";
   }
 }
