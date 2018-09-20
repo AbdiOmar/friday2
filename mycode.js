3964e7d24aba118a569c6197fc5121bf6117723d
AbdiOmar <abdiwahab1783@gmail.com>
2018-09-20 15:13:16 +0300

    index.html add styling and mode.js

--- mycode.js
+++ mycode.js
@@ -1,17 +1,19 @@
-function triangle(){
-
-
-
-var side1 = parseInt(document.getElementById("First_side").value));
+function triangle()
+{
+var side1 = parseInt(document.getElementById("First_side").value);
 var side2 = parseInt(document.getElementById("Second_side").value);
 var side3 = parseInt(document.getElementById("Third_side").value);
+
 if (side1==side2 && side1==side3 && side2 ==side3){
-  documentgetElementById('').innerHTML="it's an Equilateral Triangle"
+  document.getElementById('message').innerHTML="it's an Equilateral Triangle";
 }
 else if(side1==side2 || side2 == side3 || side3 == side1){
-  documentgetElementById('').innerHTML="it's an Isoceles Triangle"
+  document.getElementById('message').innerHTML="it's an Isoceles Triangle";
 }
 else if (side1!==side2!==side3){
-  documentgetElementById('').innerHTML="It's a Scalene Triangle"
+  document.getElementById('message').innerHTML="It's a Scalene Triangle";
 }
+  else{
+    document.getElementById('message').innerHTML="It's not Triangle";
+  }
 }
